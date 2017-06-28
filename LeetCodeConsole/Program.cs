92a88cda776c377fb8c5993cc25bdd8b7031f23c
SonOfMagic <example@same.com>
2017-06-28 20:17:57 +0800

    Add project files.

---
+++ LeetCodeConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
